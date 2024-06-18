7342356d42d6de7156f1bb49b0c1caf981750c4d
sangam shinde <sangampshinde@gmail.com>
2024-06-18 19:15:06 +0530

    JQuery learning

---
+++ script.js
@@ -0,0 +1,27 @@
+// $(document).ready(function() {
+//     $("button").click(function() {
+//         $("p").text('hello there!');
+//     });
+// });
+
+
+// selectors
+
+
+// $(document).ready(function() {
+//     $("#mark").css("background", "yellow");
+// });
+
+// $(document).ready(function() {
+//         $(".mark").css("background", "yellow");
+//     });
+
+
+// Events
+
+// to execute some JavaScript code when the DOM is ready
+$(document).ready(function(){
+    // Code to be executed
+    alert("Hello World!");
+});
+
